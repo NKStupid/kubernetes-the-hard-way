d8477fd996ee8990ce89e5fbd0f7a7f0921d397f
NKStupid <64253633+NKStupid@users.noreply.github.com>
2020-09-05 10:05:33 +0800

    Update 00.distribute_all_file.sh

--- docs/00.distribute_all_file.sh
+++ docs/00.distribute_all_file.sh
@@ -3,3 +3,10 @@ for instance in controller-0 controller-1 controller-2; do
     service-account-key.pem service-account.pem ${instance}:~/
 done
 
+for instance in controller-0 controller-1 controller-2; do
+  gcloud compute scp admin.kubeconfig kube-controller-manager.kubeconfig kube-scheduler.kubeconfig ${instance}:~/
+done
+
+for instance in controller-0 controller-1 controller-2; do
+  gcloud compute scp encryption-config.yaml etcd-v3.4.10-linux-amd64.tar.gz ${instance}:~/
+done
